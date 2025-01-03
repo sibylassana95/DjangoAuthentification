e20fb07276ccefacf06171d82f551f939dd898dd
sibylassana95 <sibyamara95@gmail.com>
2025-01-03 11:57:57 +0000

    Ajout de la gestion des environnements pour la base de données dans settings.py

--- AuthenticationProject/settings.py
+++ AuthenticationProject/settings.py
@@ -66,14 +66,26 @@ WSGI_APPLICATION = 'AuthenticationProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+ENVIRONMENT = os.getenv('DJANGO_ENV', 'production')  # Par défaut, local
+
+if ENVIRONMENT == 'local':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
-
+else:  # Production
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
