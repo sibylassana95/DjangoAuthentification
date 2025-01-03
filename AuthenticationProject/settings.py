0274143c211ac9d7cae37229d8b980197d11b469
sibylassana95 <sibyamara95@gmail.com>
2025-01-03 12:43:54 +0000

    Mise à jour du workflow de déploiement pour installer les dépendances à partir de requirements.txt et correction de l'indentation des paramètres de base de données en production

--- AuthenticationProject/settings.py
+++ AuthenticationProject/settings.py
@@ -77,14 +77,14 @@ if ENVIRONMENT == 'local':
     }
 else:  # Production
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT'),
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
     }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
